7631ade20c180e7ec16c046c12990990bd2687be
Cameron Cebula <camred9@gmail.com>
2023-08-07 17:42:10 -0400

    fixing css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "index.css";
+import "./index.css";
 import Dashboard from "./components/Dashboard";
 import StockContext from "./context/StockContext";
 import ThemeContext from "./context/ThemeContext";
